c396587c148a87745b7086dd23c337956fc948dc
Nurtimax <maksatbeknurtilek@gmail.com>
2025-08-01 20:48:05 +0600

    feat: add Swagger support for API documentation

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { TelegramConfig } from './helpers/config/services/telegram.config';
 import { AllExceptionsFilter } from './exception';
 import { AppConfig } from './helpers/config/services/app.config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,15 @@ async function bootstrap() {
   const appConfig = app.get(AppConfig);
   const telegramConfig = app.get(TelegramConfig);
   app.useGlobalFilters(new AllExceptionsFilter(telegramConfig));
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   const port = appConfig.port;
 
--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty()
   telegram_id: string;
+  @ApiProperty()
   first_name?: string;
+  @ApiProperty()
   last_name?: string;
+  @ApiProperty()
   username?: string;
 }
