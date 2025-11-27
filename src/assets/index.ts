b40caca5c6165e74a3ac5faf78efee39ad5e6215
Nurtimax <maksatbeknurtilek@gmail.com>
2025-11-27 22:46:21 +0600

    save

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,6 +2,6 @@ export const assets = {
   xbet: {
     id: 'src/assets/images/xbet/id.jpeg',
     mobcash: 'src/assets/images/xbet/mobcash.jpg',
-    mobcash_number: 'src/assets/images/xbet/mobcash-number.jpeg',
+    mobcash_number: 'src/assets/images/xbet/mobcash-number.jpg',
   },
 };
